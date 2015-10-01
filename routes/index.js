9c96f38bbcc39b3ed75586ea9125c86fcf7138cb
Giorgix <jackosinapsis@gmail.com>
2015-10-01 12:23:52 -0400

    fixed pjax page titles

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-function render(template, req, res) {
-  //if (req.params.locale) res.cookie('locale', req.params.locale);
+function render(template, data, req, res) {
   if ( req.headers['x-pjax']) {
-    res.render(template, {layout: null});
+    data.layout = null;
+    res.render(template, data);
   }
   else {
-    res.render(template);
+    res.render(template, data);
   };
 
 }
@@ -30,47 +30,48 @@ router.param('locale', function(req, res, next, locale) {
 
 router.get('/', function(req, res) {
   //console.log(req);
-  render('index', req, res);
+  render('index', { title: 'Finca El Molar - ' + req.__('title') }, req, res);
 });
 
 router.get('/:locale?', function(req, res, next) {
   if (req.params.locale == 'es' || req.params.locale == 'en') {
-    render('index', req, res);
+    render('index', { title: 'Finca El Molar - ' + req.__('title') }, req, res);
   }
   else next();
 })
 
 router.get('/:locale?/wines', function(req, res) {
   //console.log(req);
-   render('products', req, res);
+   render('products', { title: 'Finca El Molar - ' + req.__('our wines') }, req, res);
 });
 
 router.get('/:locale?/wines/quantum', function(req, res) {
-   render('quantum', req, res);
+  console.log(req);
+   render('quantum', { title: 'Finca El Molar - ' + req.__('our wines') +  ' |  Quantum 2012' }, req, res);
 });
 
 router.get('/:locale?/wines/finca', function(req, res) {
-   render('finca', req, res);
+   render('finca', { title: 'Finca El Molar - ' + req.__('our wines') +  ' |  Finca el molar 2013' }, req, res);
 });
 
 router.get('/:locale?/wines/quantum-ice', function(req, res) {
-   render('quantum-ice', req, res);
+   render('quantum-ice', { title: 'Finca El Molar - ' + req.__('our wines') + ' | ' +  req.__('quantum hielo') }, req, res);
 });
 
 router.get('/:locale?/tourism', function(req, res) {
-   render('tourism', req, res);
+   render('tourism',  { title: 'Finca El Molar' + ' | ' + req.__('roural tourism') }, req, res);
 });
 
 router.get('/:locale?/about', function(req, res) {
-   render('about', req, res);
+   render('about', { title: 'Finca El Molar' + ' | ' + req.__('our history') }, req, res);
 });
 
 router.get('/:locale?/organic-production', function(req, res) {
-   render('organic', req, res);
+   render('organic',  { title: 'Finca El Molar' + ' | ' + req.__('organic production') },req, res);
 });
 
 router.get('/:locale?/winery', function(req, res) {
-   render('winery', req, res);
+   render('winery',  { title: 'Finca El Molar' + ' | ' + req.__('winery and wineyards') },req, res);
 });
 
 
